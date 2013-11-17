55e7ab673ccc458877fd46a49ec4f6339a5b1869
Ryan Greenberg <ryangreenberg@gmail.com>
2013-11-17 12:54:55 -0800

    Add headers to responses when add_headers is true

--- lib/rack/metadata.rb
+++ lib/rack/metadata.rb
@@ -1,13 +1,36 @@
 require "rack/metadata/version"
+require "rack/metadata/config"
 
 module Rack
   class Metadata
-    def initialize(app)
+    def self.header_name(str)
+      "X-" + str.to_s.split(/[-_ ]/).map(&:capitalize).join("-")
+    end
+
+    def self.header_value(obj)
+      obj.to_s
+    end
+
+    attr_reader :app, :config, :metadata
+
+    def initialize(app, hsh_or_config = {})
       @app = app
+      @config = Config.from(hsh_or_config)
+      @metadata_headers = metadata_headers(@config.metadata)
     end
 
     def call(env)
-      @app.call(env)
+      @app.call(env).tap do |status, headers, body|
+        headers.merge!(@metadata_headers) if config.add_headers?(env, [status, headers, body])
+      end
+    end
+
+    private
+
+    def metadata_headers(hsh)
+      Hash[@config.metadata.map do |k, v|
+        [self.class.header_name(k), self.class.header_value(v)]
+      end]
     end
   end
 end
--- spec/metadata_spec.rb
+++ spec/metadata_spec.rb
@@ -1,15 +1,81 @@
 require 'spec_helper'
 
 describe Rack::Metadata do
-  it "conforms to the Rack spec" do
-    app = Rack::Builder.new do
-      use Rack::Lint
-      use Rack::Metadata
-      use Rack::Lint
-      run lambda { |env| [200, {'Content-Type' => 'text/plain'}, ['OK']] }
+  def base_metadata
+    {:some_key => :some_value}
+  end
+
+  def base_config(hsh = base_metadata)
+    Rack::Metadata::Config.from(hsh)
+  end
+
+  describe "#call" do
+    it "can be constructed with a hash instead of a config object" do
+      hsh = {:color => "Red", :virtue => "Beauty"}
+      app = rack_app(OK_APP, Rack::Metadata, hsh)
+      rsp = app.call(rack_env)
+      rsp.headers.should include({"X-Color" => "Red"}, {"X-Virtue" => "Beauty"})
     end
 
-    env = Rack::MockRequest.env_for("/")
-    app.call(env)
+    context "when config.enabled? returns false" do
+      it "does not add headers"
+      it "does not modify HTML"
+      it "calls the underlying app when config.path is requested"
+    end
+
+    context "when config.add_headers? returns true" do
+      it "adds the metadata pairs as response headers" do
+        config = Rack::Metadata::Config.new do |config|
+          config.metadata = {:color => "Blue", :virtue => "Justice"}
+        end
+        config.stub(:add_headers?).and_return(true)
+        app = rack_app(OK_APP, Rack::Metadata, config)
+        rsp = app.call(rack_env)
+        rsp.headers.should include({"X-Color" => "Blue"}, {"X-Virtue" => "Justice"})
+      end
+    end
+
+    context "when config.add_headers? returns false" do
+      it "does not modify the response headers" do
+        env = rack_env
+        config = base_config
+        config.stub(:add_headers?).and_return(false)
+        app = rack_app(OK_APP, Rack::Metadata, config)
+        rsp = app.call(env)
+        rsp.headers.should == unchanged_rsp(OK_APP, env).headers
+      end
+    end
+  end
+
+  describe ".header_name" do
+    it "prepends X-" do
+      Rack::Metadata.header_name("Dog").should == "X-Dog"
+    end
+
+    it "capitalizes words" do
+      Rack::Metadata.header_name("cat").should == "X-Cat"
+    end
+
+    it "converts symbols to strings" do
+      Rack::Metadata.header_name(:giraffe).should == "X-Giraffe"
+    end
+
+    it "converts spaces to dashes" do
+      Rack::Metadata.header_name("mountain lion").should == "X-Mountain-Lion"
+    end
+
+    it "converts underscores to dashes" do
+      Rack::Metadata.header_name("mountain_lion").should == "X-Mountain-Lion"
+    end
+  end
+
+  describe ".header_value" do
+    it "converts to a string" do
+      actual_values = [1, true, :true, nil]
+      expected_values = ["1", "true", "true", ""]
+      actual_values.zip(expected_values).each do |actual, expected|
+        Rack::Metadata.header_value(actual).should == expected
+      end
+    end
   end
 end
