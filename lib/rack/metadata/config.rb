b97ca7d9656b95a6ab95c7b6007a3677b9c2dd2a
Ryan Greenberg <ryangreenberg@gmail.com>
2013-11-17 14:47:22 -0800

    Document config options

--- lib/rack/metadata/config.rb
+++ lib/rack/metadata/config.rb
@@ -1,6 +1,44 @@
 class Rack::Metadata
   class Config
-    attr_accessor :metadata, :add_headers, :add_html_comment, :is_enabled, :path, :insert_html_after
+    # You can create a configuration by providing a block to the constructor,
+    # or by setting values directly on a new instance:
+    #
+    # Rack::Metadata::Config.new do |config|
+    #   config.add_html_comment = false
+    # end
+    #
+    # config = Rack::Metadata::Config.new
+    # config.add_html_comment = false
+    #
+    # Configuration options:
+    #
+    # metadata: a hash of key/value pairs that will be added as X- headers,
+    # HTML content, or exposed directly at a JSON endpoint, depending on the
+    # other configuration (default: {})
+    #
+    # is_enabled: whether or not this middleware will add any metadata to the
+    # response. It can be boolean value, or an object that responds to .call
+    # with a boolean value. The Rack request environment is provided to a
+    # callable object. This can be useful for adding data only to requests
+    # from a certain IP block, for example. (default: true)
+    #
+    # add_headers: whether or not metadata will be added to this request as X-
+    # headers. It can be a boolean value, or an objects that responds to .call
+    # with a boolean value. The Rack request environment *and* current Rack
+    # response tuple are provided to a callable object. (default: true)
+    #
+    # add_html_comment: whether or not metadata will be added to this request
+    # as HTML. It can be a boolean value, or an objects that responds to .call
+    # with a boolean value. The Rack request environment *and* current Rack
+    # response tuple are provided to a callable object. Note: content is only
+    # added to responses of content-type text/html. (default: true)
+    #
+    # insert_html_after: the HTML tag after which the HTML metadata will be
+    # added. (default: </body>)
+    #
+    # path: an endpoint at which metadata will be returned as a JSON string.
+    # Set to nil to disable. (default: nil)
+    attr_accessor :metadata, :is_enabled, :add_headers, :add_html_comment, :insert_html_after, :path
 
     def self.from(obj)
       obj.is_a?(self) ? obj : self.new {|cnf| cnf.metadata = obj }
