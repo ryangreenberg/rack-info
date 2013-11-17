b8acf3d1edf910048a3a9c54e7029c16d0791674
Ryan Greenberg <ryangreenberg@gmail.com>
2013-11-17 12:54:06 -0800

    Add helpers for testing (middleware)#call

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,3 +6,41 @@ require 'rack/builder'
 
 require 'rspec'
 require 'rspec/autorun'
+
+# Simple Rack apps for testing
+OK_APP = lambda {|env| [200, {'Content-Type' => 'text/plain'}, ['OK']] }
+NOT_FOUND_APP = lambda {|env| [400, {'Content-Type' => 'html'}, ['Not found']] }
+
+module RackSpecHelpers
+  def rack_env(path="/")
+    Rack::MockRequest.env_for(path)
+  end
+
+  # Construct a middleware chain with +underlying_app+ at the bottom,
+  # Rack::Lint on either side of +middleware+, and +middleware_args+
+  # provided when constructing +middleware+.
+  #
+  # For more convenient assertions, the response is automatically
+  # wrapped as a Rack::MockResponse instead of a [status, header, body]
+  # tuple.
+  def rack_app(underlying_app, middleware, *middleware_args)
+    app = Rack::Builder.new do
+      use Rack::Lint
+      use middleware, *middleware_args
+      use Rack::Lint
+      run underlying_app
+    end.to_app
+
+    lambda do |env|
+      Rack::MockResponse.new(*app.call(env))
+    end
+  end
+
+  def unchanged_rsp(app, env)
+    Rack::MockResponse.new(*app.call(env))
+  end
+end
+
+RSpec.configure do |config|
+  config.include RackSpecHelpers
+end
